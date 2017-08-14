201b0d00f628b16885f340a53fd63380a3d3b333
Alberto Morales <kodenix@gmail.com>
2017-08-15 00:57:13 +0200

    Cambiando puerto

--- index.js
+++ index.js
@@ -22,5 +22,5 @@ const options = {
 app.use(express.static(path.join(__dirname,'html'), options));
 
 /** Arranque del servidor */
-app.listen(8080);
+app.listen(80);
 console.log('listening on port 8080');
